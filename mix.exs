514d95d2b0af54a5bc8e8cf9b8e54ff4f7680630
Tim Mecklem <timothy@mecklem.com>
2019-04-17 06:37:12 -0400

    Add GitHub releases url and bump version

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule NervesSystemRpi3.MixProject do
   defp nerves_package do
     [
       type: :system,
-      # artifact_sites: [
-      #   {:github_releases, "nerves-project/#{@app}"}
-      # ],
+      artifact_sites: [
+        {:github_releases, "makerion/#{@app}"}
+      ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
